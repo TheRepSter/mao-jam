cd63576629b79fa5be421d2653d0acef1dace151
Arnau del Río <arnau.delrio@gmail.com>
2026-02-18 11:51:19 +0100

    Fixing issues on my computer, giving permissions to the sh script and adding name-main guard to simulator_combined_strategies

--- base/logger.py
+++ base/logger.py
@@ -85,7 +85,7 @@ def handle_exception(exc_type, exc_value, exc_traceback):
         sys.__excepthook__(exc_type, exc_value, exc_traceback)
         return
 
-    configure_logging().error(
+    configure_logging(file_log_path="uncaught_exceptions.log").error(
         "Uncaught exception",
         exc_info=(exc_type, exc_value, exc_traceback),
     )
